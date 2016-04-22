70113c5928e1d5a6d3bfbaf225c5ae6693ec656b
Hamdi Douss <douss.hamdi@gmail.com>
2016-04-22 14:38:56 +0100

    Line intersect point

---
+++ aljebra/src/main/java/com/aljebra/scalar/condition/Or.java
@@ -0,0 +1,64 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.aljebra.scalar.condition;
+
+import com.aljebra.field.Field;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import java.util.Arrays;
+
+/**
+ * A predicate that is true if one of the given predicates is true.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class Or implements Predicate {
+
+    /**
+     * Predicates.
+     */
+    private final Multiset<Predicate> opers;
+
+    /**
+     * Constructor.
+     * @param operands Predicates
+     */
+    public Or(final Predicate... operands) {
+        this.opers = HashMultiset.create(Arrays.asList(operands));
+    }
+
+    @Override
+    public boolean resolve(final Field<?> field) {
+        boolean result = false;
+        for (final Predicate predicate : this.opers) {
+            if (predicate.resolve(field)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
