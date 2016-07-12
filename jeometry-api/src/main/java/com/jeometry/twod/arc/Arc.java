807085c6a9768d507857fe222f8d4b69075502f7
Hamdi Douss <douss.hamdi@gmail.com>
2016-07-12 17:50:19 +0100

    arcs

---
+++ jeometry-api/src/main/java/com/jeometry/twod/arc/Arc.java
@@ -0,0 +1,65 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.arc;
+
+import com.aljebra.scalar.Scalar;
+import com.aljebra.vector.Vect;
+import com.jeometry.twod.Renderable;
+
+/**
+ * Arc interface describing an arc by a center, a radius, a starting angle
+ * and an ending angle.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public interface Arc extends Renderable {
+
+    /**
+     * Gives the center of the arc.
+     * @return Arc center
+     */
+    Vect center();
+
+    /**
+     * Gives the arc radius.
+     * @return Arc radius
+     */
+    Scalar radius();
+
+    /**
+     * Gives the arc starting angle measure (as defined according
+     * to trigonometric circle).
+     * @return Arc starting angle
+     */
+    Number start();
+
+    /**
+     * Gives the arc ending angle measure (as defined according
+     * to trigonometric circle).
+     * @return Arc ending angle
+     */
+    Number end();
+
+}
---
+++ jeometry-api/src/main/java/com/jeometry/twod/arc/RadAngleArc.java
@@ -0,0 +1,96 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.arc;
+
+import com.aljebra.scalar.Scalar;
+import com.aljebra.vector.Vect;
+import lombok.ToString;
+
+/**
+ * Arc implementation class describing an arc by a center, a radius,
+ * a starting and an ending angle.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+@ToString
+public class RadAngleArc implements Arc {
+
+    /**
+     * Arc radius.
+     */
+    private final Scalar rdx;
+
+    /**
+     * Arc center.
+     */
+    private final Vect cntr;
+
+    /**
+     * Arc starting angle.
+     */
+    private final Number starting;
+
+    /**
+     * Arc ending angle.
+     */
+    private final Number ending;
+
+    /**
+     * Ctor.
+     * @param radius Arc radius
+     * @param center Arc center
+     * @param start Arc starting angle
+     * @param end Arc ending angle
+     * @checkstyle ParameterNumberCheck (3 lines)
+     */
+    public RadAngleArc(final Vect center, final Scalar radius,
+        final Number start, final Number end) {
+        this.rdx = radius;
+        this.cntr = center;
+        this.starting = start;
+        this.ending = end;
+    }
+
+    @Override
+    public final Vect center() {
+        return this.cntr;
+    }
+
+    @Override
+    public final Scalar radius() {
+        return this.rdx;
+    }
+
+    @Override
+    public final Number start() {
+        return this.starting;
+    }
+
+    @Override
+    public final Number end() {
+        return this.ending;
+    }
+
+}
