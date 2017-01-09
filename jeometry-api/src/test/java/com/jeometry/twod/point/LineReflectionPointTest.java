f4b7861ebb44f449e36a215e927dbb55aea6e6b3
Hamdi Douss <douss.hamdi@gmail.com>
2017-01-09 18:04:35 +0100

    Line reflection point

---
+++ jeometry-api/src/test/java/com/jeometry/twod/point/LineReflectionPointTest.java
@@ -0,0 +1,65 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.point;
+
+import com.aljebra.field.Field;
+import com.aljebra.field.impl.doubles.Decimal;
+import com.aljebra.vector.Vect;
+import com.jeometry.twod.line.Line;
+import com.jeometry.twod.line.PerpBisector;
+import com.jeometry.twod.line.PtsLine;
+import com.jeometry.twod.line.analytics.Equal;
+import com.jeometry.twod.segment.PtsSegment;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Tests for {@link LineReflectionPoint}.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class LineReflectionPointTest {
+
+    /**
+     * {@link LineReflectionPoint} can build a line reflected point such as
+     * the symmetry axis is the bisector of the segment.
+     */
+    @Test
+    public void buildsReflectedPoint() {
+        final Vect input = new RandomPoint();
+        final Line line = new PtsLine(new RandomPoint(), new RandomPoint());
+        final Field<Double> field = new Decimal();
+        MatcherAssert.assertThat(
+            new Equal(
+                new PerpBisector(
+                    new PtsSegment(input, new LineReflectionPoint(line, input))
+                ), line
+            ).resolve(field),
+            Matchers.is(true)
+        );
+    }
+
+}
