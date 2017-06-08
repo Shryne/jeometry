c093e11bb96039587b70eb3a9ca29060f75a396a
Hamdi Douss <douss.hamdi@gmail.com>
2017-06-08 11:54:58 +0100

    Line Reflection line.

---
+++ jeometry-api/src/test/java/com/jeometry/twod/line/LineReflectionLineTest.java
@@ -0,0 +1,70 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.line;
+
+import com.aljebra.field.Field;
+import com.aljebra.field.impl.doubles.Decimal;
+import com.aljebra.vector.Sum;
+import com.aljebra.vector.VectEquals;
+import com.jeometry.twod.point.LineReflectionPoint;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Tests for {@link LineReflectionLine}.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class LineReflectionLineTest {
+
+    /**
+     * {@link LineReflectionLine} can build the reflection line given
+     * the reflection axis.
+     */
+    @Test
+    public void buildsReflection() {
+        final Line origin = new RandomLine();
+        final Line axis = new RandomLine();
+        final Line result = new LineReflectionLine(axis, origin);
+        final Field<Double> dec = new Decimal();
+        MatcherAssert.assertThat(
+            new VectEquals(
+                result.point(), new LineReflectionPoint(axis, origin.point())
+            ).resolve(dec),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new VectEquals(
+                new Sum(result.point(), result.direction()),
+                new LineReflectionPoint(
+                    axis, new Sum(origin.point(), origin.direction())
+                )
+            ).resolve(dec),
+            Matchers.is(true)
+        );
+    }
+
+}
