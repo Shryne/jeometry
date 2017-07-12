60840be2ae1aa7337e8b6ac4f13c0959e50f8b3b
Hamdi Douss <douss.hamdi@gmail.com>
2017-07-12 14:23:51 +0100

    Perpendicular predicate

---
+++ jeometry-api/src/main/java/com/jeometry/twod/line/analytics/Perpendicular.java
@@ -0,0 +1,87 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.line.analytics;
+
+import com.aljebra.field.Field;
+import com.aljebra.field.MetricSpaceField;
+import com.aljebra.metric.angle.VectsDegrees;
+import com.aljebra.scalar.condition.Predicate;
+import com.aljebra.vector.Vect;
+import com.jeometry.twod.line.Line;
+
+/**
+ * A predicate to determine if two vectors or lines are perpendicular,
+ * according to euclidean geometry.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class Perpendicular implements Predicate {
+
+    /**
+     * First vector.
+     */
+    private final Vect first;
+
+    /**
+     * Second vector.
+     */
+    private final Vect second;
+
+    /**
+     * Constructor.
+     * @param first First line
+     * @param second Second line
+     */
+    public Perpendicular(final Line first, final Line second) {
+        this(first.direction(), second.direction());
+    }
+
+    /**
+     * Constructor.
+     * @param first First vector
+     * @param second Second vector
+     */
+    public Perpendicular(final Vect first, final Vect second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean resolve(final Field<?> field) {
+        if (field instanceof MetricSpaceField<?>) {
+            final Double degrees = new VectsDegrees(
+                this.first, this.second
+            ).resolve(((MetricSpaceField<?>) field).product()).doubleValue();
+            final double error = 1.e-6;
+            return Math.abs(degrees - Math.PI / 2) < error
+                || Math.abs(degrees + Math.PI / 2) < error;
+        } else {
+            throw new UnsupportedOperationException(
+                String.format("Field %s is not a metric space field", field)
+            );
+        }
+    }
+
+}
---
+++ jeometry-api/src/test/java/com/jeometry/twod/line/analytics/PerpendicularTest.java
@@ -0,0 +1,82 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.line.analytics;
+
+import com.aljebra.field.impl.doubles.Decimal;
+import com.jeometry.twod.line.Line;
+import com.jeometry.twod.line.ParallelLine;
+import com.jeometry.twod.line.PerpLine;
+import com.jeometry.twod.line.RandomLine;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Tests for {@link Perpendicular}.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class PerpendicularTest {
+
+    /**
+     * {@link Perpendicular} resolves to true when lines are perpendicular.
+     */
+    @Test
+    public void resolvesTrueIfPerp() {
+        final Line any = new RandomLine();
+        MatcherAssert.assertThat(
+            new Perpendicular(any, new PerpLine(any)).resolve(new Decimal()),
+            Matchers.is(true)
+        );
+    }
+
+    /**
+     * {@link Perpendicular} resolves to false when lines are parallel.
+     */
+    @Test
+    public void resolvesfalseIfParallel() {
+        final Line any = new RandomLine();
+        MatcherAssert.assertThat(
+            new Perpendicular(
+                any, new ParallelLine(any)
+            ).resolve(new Decimal()),
+            Matchers.is(false)
+        );
+    }
+
+    /**
+     * {@link Perpendicular} can accept lines or vectors.
+     */
+    @Test
+    public void acceptsLinesOrVects() {
+        final Line any = new RandomLine();
+        MatcherAssert.assertThat(
+            new Perpendicular(
+                any.direction(), new PerpLine(any).direction()
+            ).resolve(new Decimal()),
+            Matchers.is(true)
+        );
+    }
+}
