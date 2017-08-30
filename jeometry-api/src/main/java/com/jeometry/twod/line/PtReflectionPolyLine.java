29b8b13b6f4404358bd412a2396547505e9fc6f7
Hamdi Douss <douss.hamdi@gmail.com>
2017-08-30 12:14:31 +0100

    PolyLine reflection across a point

---
+++ jeometry-api/src/main/java/com/jeometry/twod/line/PtReflectionPolyLine.java
@@ -0,0 +1,83 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.line;
+
+import com.aljebra.scalar.AddIdentity;
+import com.aljebra.vector.Vect;
+import com.jeometry.twod.point.PtReflectionPoint;
+import com.jeometry.twod.point.XyPoint;
+import java.util.List;
+import lombok.ToString;
+
+/**
+ * A polyline defined as the reflection of a polyline, across a given point.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+@ToString(includeFieldNames = false)
+public final class PtReflectionPolyLine extends PtsPolyline {
+
+    /**
+     * Constructor.
+     * @param center Reflection center
+     * @param input The polyline to reflect
+     */
+    public PtReflectionPolyLine(final Vect center, final Polyline input) {
+        super(PtReflectionPolyLine.reflected(center, input.points()));
+    }
+
+    /**
+     * Constructor. Builds the reflection polyline across the origin.
+     * @param input The line to reflect
+     */
+    public PtReflectionPolyLine(final Polyline input) {
+        this(new XyPoint(new AddIdentity(), new AddIdentity()), input);
+    }
+
+    /**
+     * Builds a list of point-reflected points across a center.
+     * @param center Reflection center
+     * @param pts The points to reflect
+     * @return An array of reflected points
+     */
+    private static Vect[] reflected(final Vect center, final List<Vect> pts) {
+        final int size = pts.size();
+        final Vect[] result = new Vect[size];
+        for (int idx = 0; idx < size; ++idx) {
+            result[idx] = PtReflectionPolyLine.reflected(center, pts.get(idx));
+        }
+        return result;
+    }
+
+    /**
+     * Builds a point-reflected point across a center.
+     * @param center Reflection center
+     * @param point The point to reflect
+     * @return The reflected point
+     */
+    private static Vect reflected(final Vect center, final Vect point) {
+        return new PtReflectionPoint(center, point);
+    }
+}
---
+++ jeometry-api/src/test/java/com/jeometry/twod/line/PtReflectionPolyLineTest.java
@@ -0,0 +1,114 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2016, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.line;
+
+import com.aljebra.field.impl.doubles.Decimal;
+import com.aljebra.vector.Vect;
+import com.aljebra.vector.VectEquals;
+import com.jeometry.twod.point.PtReflectionPoint;
+import com.jeometry.twod.point.RandomPoint;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Tests for {@link PtReflectionPolyLine}.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class PtReflectionPolyLineTest {
+
+    /**
+     * {@link PtReflectionPolyLine} can build the reflection polyline given
+     * the reflection center.
+     */
+    @Test
+    public void buildsReflection() {
+        final Polyline input = new PtsPolyline(
+            new RandomPoint(), new RandomPoint(), new RandomPoint(),
+            new RandomPoint()
+        );
+        final RandomPoint across = new RandomPoint();
+        final Polyline result = new PtReflectionPolyLine(across, input);
+        for (int idx = 0; idx < result.points().size(); ++idx) {
+            PtReflectionPolyLineTest.equal(
+                result.points().get(idx),
+                PtReflectionPolyLineTest.reflected(
+                    across, input.points().get(idx)
+                )
+            );
+        }
+    }
+
+    /**
+     * {@link PtReflectionPolyLine} can build the reflection polyline
+     * across the origin if not given the center.
+     */
+    @Test
+    public void buildsReflectionAcrossOrigin() {
+        final Polyline input = new PtsPolyline(
+            new RandomPoint(), new RandomPoint(), new RandomPoint(),
+            new RandomPoint()
+        );
+        final Polyline result = new PtReflectionPolyLine(input);
+        for (int idx = 0; idx < result.points().size(); ++idx) {
+            PtReflectionPolyLineTest.equal(
+                result.points().get(idx),
+                PtReflectionPolyLineTest.reflected(input.points().get(idx))
+            );
+        }
+    }
+
+    /**
+     * Asserts equality of two points.
+     * @param first First point
+     * @param second Second point
+     */
+    private static void equal(final Vect first, final Vect second) {
+        MatcherAssert.assertThat(
+            new VectEquals(first, second).resolve(new Decimal()),
+            Matchers.is(true)
+        );
+    }
+
+    /**
+     * Builds a point-reflected point across a center.
+     * @param center Reflection center
+     * @param point The point to reflect
+     * @return The reflected point
+     */
+    private static Vect reflected(final Vect center, final Vect point) {
+        return new PtReflectionPoint(center, point);
+    }
+
+    /**
+     * Builds a point-reflected point across the origin.
+     * @param point The point to reflect
+     * @return The reflected point
+     */
+    private static Vect reflected(final Vect point) {
+        return new PtReflectionPoint(point);
+    }
+}
