f2b33db92f684a23204107126eb1fb1d39ddd673
Hamdi Douss <douss.hamdi@gmail.com>
2017-09-11 13:54:35 +0100

    Remove extra space

--- jeometry-api/src/main/java/com/jeometry/twod/segment/PtsSegment.java
+++ jeometry-api/src/main/java/com/jeometry/twod/segment/PtsSegment.java
@@ -33,7 +33,7 @@ import lombok.ToString;
  * @since 0.1
  */
 @ToString(includeFieldNames = false)
-public  class PtsSegment implements Segment {
+public class PtsSegment implements Segment {
 
     /**
      * First segment extremity.
