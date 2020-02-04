5e0c21dcbd0685cda8f7c810cfda624fdba95f3e
Hamdi Douss <douss.hamdi@gmail.com>
2020-02-04 20:46:41 +0100

    #37 add tests for FixedColorFill, FixedStroke and FixedStyle

---
+++ jeometry-api/src/test/java/com/jeometry/twod/style/impl/FixedStyleTest.java
@@ -0,0 +1,54 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2020, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.style.impl;
+
+import com.jeometry.twod.style.Fill;
+import com.jeometry.twod.style.Stroke;
+import com.jeometry.twod.style.Style;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Tests for {@link FixedStyle}.
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class FixedStyleTest {
+
+    /**
+     * {@link FixedStyle} responds with correct fill style and stroke style.
+     */
+    @Test
+    public void respondsWithCorrectAttributes() {
+        final Fill fill = Mockito.mock(Fill.class);
+        final Stroke stroke = Mockito.mock(Stroke.class);
+        final Style style = new FixedStyle(stroke, fill);
+        MatcherAssert.assertThat(style.fillStyle(), Matchers.equalTo(fill));
+        MatcherAssert.assertThat(style.strokeStyle(), Matchers.equalTo(stroke));
+    }
+
+}
