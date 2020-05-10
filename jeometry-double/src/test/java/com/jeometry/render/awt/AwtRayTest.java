ce307c02a5b28d0881755d4bb3f115d6f460ee71
Hamdi Douss <douss.hamdi@gmail.com>
2020-05-10 16:01:44 +0100

    #109 ray mocks

--- jeometry-double/src/test/java/com/jeometry/render/awt/AwtRayTest.java
+++ jeometry-double/src/test/java/com/jeometry/render/awt/AwtRayTest.java
@@ -27,7 +27,8 @@ import com.aljebra.field.impl.doubles.Decimal;
 import com.jeometry.model.decimal.DblPoint;
 import com.jeometry.twod.Shape;
 import com.jeometry.twod.mock.SpyAngle;
-import com.jeometry.twod.ray.Ray;
+import com.jeometry.twod.mock.SpyRay;
+import com.jeometry.twod.ray.PtDirRay;
 import java.awt.Graphics2D;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -43,69 +44,65 @@ public final class AwtRayTest {
     /**
      * {@link AwtRay} renders forward rays.
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void rendersForwardRays() {
-        final Ray<Double> ray = Mockito.mock(Ray.class);
-        Mockito.when(ray.origin()).thenReturn(new DblPoint(0., 0.));
-        Mockito.when(ray.direction()).thenReturn(new DblPoint(1., 1.));
+        final SpyRay<Double> ray = new SpyRay<>(
+            new PtDirRay<>(new DblPoint(0., 0.), new DblPoint(1., 1.))
+        );
         final AwtRay painter = new AwtRay(new Decimal());
         painter.setContext(new AwtDrawableSurface().context());
         painter.setGraphics(Mockito.mock(Graphics2D.class));
         painter.render(new Shape(ray));
-        Mockito.verify(ray, Mockito.atLeastOnce()).direction();
-        Mockito.verify(ray, Mockito.atLeastOnce()).origin();
+        MatcherAssert.assertThat(ray.directioned(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(ray.origined(), Matchers.equalTo(true));
     }
 
     /**
      * {@link AwtRay} renders backward rays.
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void rendersBackwardRays() {
-        final Ray<Double> ray = Mockito.mock(Ray.class);
-        Mockito.when(ray.origin()).thenReturn(new DblPoint(0., 0.));
-        Mockito.when(ray.direction()).thenReturn(new DblPoint(-1., -1.));
+        final SpyRay<Double> ray = new SpyRay<>(
+            new PtDirRay<>(new DblPoint(0., 0.), new DblPoint(-1., -1.))
+        );
         final AwtRay painter = new AwtRay(new Decimal());
         painter.setContext(new AwtDrawableSurface().context());
         painter.setGraphics(Mockito.mock(Graphics2D.class));
         painter.render(new Shape(ray));
-        Mockito.verify(ray, Mockito.atLeastOnce()).direction();
-        Mockito.verify(ray, Mockito.atLeastOnce()).origin();
+        MatcherAssert.assertThat(ray.directioned(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(ray.origined(), Matchers.equalTo(true));
     }
 
     /**
      * {@link AwtRay} renders vertical up rays.
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void rendersVerticalUpRays() {
-        final Ray<Double> ray = Mockito.mock(Ray.class);
-        Mockito.when(ray.origin()).thenReturn(new DblPoint(0., 0.));
-        Mockito.when(ray.direction()).thenReturn(new DblPoint(0., 1.));
+        final SpyRay<Double> ray = new SpyRay<>(
+            new PtDirRay<>(new DblPoint(0., 0.), new DblPoint(0., 1.))
+        );
         final AwtRay painter = new AwtRay(new Decimal());
         painter.setContext(new AwtDrawableSurface().context());
         painter.setGraphics(Mockito.mock(Graphics2D.class));
         painter.render(new Shape(ray));
-        Mockito.verify(ray, Mockito.atLeastOnce()).direction();
-        Mockito.verify(ray, Mockito.atLeastOnce()).origin();
+        MatcherAssert.assertThat(ray.directioned(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(ray.origined(), Matchers.equalTo(true));
     }
 
     /**
      * {@link AwtRay} renders vertical down rays.
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void rendersVerticalDownRays() {
-        final Ray<Double> ray = Mockito.mock(Ray.class);
-        Mockito.when(ray.origin()).thenReturn(new DblPoint(0., 0.));
-        Mockito.when(ray.direction()).thenReturn(new DblPoint(0., -1.));
+        final SpyRay<Double> ray = new SpyRay<>(
+            new PtDirRay<>(new DblPoint(0., 0.), new DblPoint(0., -1.))
+        );
         final AwtRay painter = new AwtRay(new Decimal());
         painter.setContext(new AwtDrawableSurface().context());
         painter.setGraphics(Mockito.mock(Graphics2D.class));
         painter.render(new Shape(ray));
-        Mockito.verify(ray, Mockito.atLeastOnce()).direction();
-        Mockito.verify(ray, Mockito.atLeastOnce()).origin();
+        MatcherAssert.assertThat(ray.directioned(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(ray.origined(), Matchers.equalTo(true));
     }
 
     /**
---
+++ jeometry-double/src/test/java/com/jeometry/twod/mock/SpyRay.java
@@ -0,0 +1,88 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2020, Hamdi Douss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jeometry.twod.mock;
+
+import com.aljebra.vector.Vect;
+import com.jeometry.twod.ray.Ray;
+
+/**
+ * Mock decorator for ray with spying (verifying) capabilities on the methods: origin and direction.
+ * @param <T> scalar types
+ * @since 0.1
+ */
+public final class SpyRay<T> implements Ray<T> {
+
+    /**
+     * Decorated ray.
+     */
+    private final Ray<T> org;
+
+    /**
+     * Boolean indicating if origin method was called.
+     */
+    private Boolean orig;
+
+    /**
+     * Boolean indicating if direction method was called.
+     */
+    private Boolean dir;
+
+    /**
+     * Ctor.
+     * @param origin Decorated ray
+     */
+    public SpyRay(final Ray<T> origin) {
+        this.org = origin;
+        this.orig = Boolean.FALSE;
+        this.dir = Boolean.FALSE;
+    }
+
+    @Override
+    public Vect<T> direction() {
+        this.dir = Boolean.TRUE;
+        return this.org.direction();
+    }
+
+    @Override
+    public Vect<T> origin() {
+        this.orig = Boolean.TRUE;
+        return this.org.origin();
+    }
+
+    /**
+     * Accessor for a boolean indicating if origin method was called.
+     * @return True if origin method was called.
+     */
+    public Boolean origined() {
+        return this.orig;
+    }
+
+    /**
+     * Accessor for a boolean indicating if direction method was called.
+     * @return True if direction method was called.
+     */
+    public Boolean directioned() {
+        return this.dir;
+    }
+}
